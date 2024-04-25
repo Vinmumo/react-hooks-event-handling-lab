6226e9cc314b16f3dc7e3fdd2bf804fffe137548
vincent <vinnymummo@gmail.com>
2024-04-25 19:22:29 +0300

    commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe (){
+
+function handleFocus(event){
+   console.log('Good!')
+}
+
+function handleOnBlur(){
+    console.log('Hey! Eyes on me!')
+}
+    return (
+        <>
+        <button onFocus={handleFocus} onBlur={handleOnBlur}>Eyes on me</button>
+        </>
+    )
+}
